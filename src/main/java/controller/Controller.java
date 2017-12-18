23edb832cd3f3ae12473b253b18aadcee48fd7be
Carolyne Peelman <peelmancarolyne@gmail.com>
2017-12-18 15:50:31 +0100

    Controller + ImageDb back-end

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,30 +1,24 @@
 package controller;
 
 import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-
 import db.ImageDb;
 
-/**
- * Servlet implementation class Controller
- */
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ImageDb imageDb;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public Controller() {
 		super();
 		imageDb = new ImageDb();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -46,20 +40,34 @@ public class Controller extends HttpServlet {
 		}
 		switch (action) {
 		case "uploadeimage":
-			destination = uploadImage(request, response);
+			uploadImage(request, response);
 			break;
 		case "downloadimage":
-			//destination = downloadImage(request, response);
+			destination = downloadImage(request, response);
 			break;
 		default:
 			break;
 		}
+		if (destination != "") {
+			RequestDispatcher rd = request.getRequestDispatcher(destination);
+			rd.forward(request, response);
+		}
 	}
 
-	private String uploadImage(HttpServletRequest request, HttpServletResponse response)
+	private void uploadImage(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		Part file = request.getPart("file");
-		
+		this.imageDb.addNewImage(file);
+		response.sendRedirect("Controller?action=imageOverview");
+	}
+
+	private String downloadImage(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String fileName = request.getParameter("fileName");
+		String fileFormat = fileName.substring(fileName.indexOf(".") + 1);
+
+		response.setHeader("Content-Disposition", "attachment; filename=\"download." + fileFormat + "\"");
+		ImageIO.write(this.imageDb.getImage(fileName), fileFormat, response.getOutputStream());
 		return null;
 	}
 
