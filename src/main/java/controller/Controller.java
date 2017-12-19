7a5ec6e735c7f4709e391249d521e4b60b039e6d
FerreVdB <ferre.vandenbroeck@student.ucll.be>
2017-12-19 13:32:54 +0100

    merge confilcten opgelost

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -16,12 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-<<<<<<< HEAD
 import db.AfdelingDb;
-=======
 import org.joda.time.DateTime;
 
->>>>>>> bb890df73486a715a19438057639c1cf37b311c9
 import db.ImageDb;
 import domain.OpenClassSession;
 import domain.Afdeling;
@@ -34,11 +31,8 @@ public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ImageDb imageDb;
 	private SimpleMail mail;
-<<<<<<< HEAD
 	private AfdelingDb afdelingDb;
-=======
 	ArrayList<Afdeling> afdelingen = new ArrayList<>();
->>>>>>> bb890df73486a715a19438057639c1cf37b311c9
 
 	public Controller() throws ClassNotFoundException, SQLException {
 		super();
@@ -182,46 +176,8 @@ public class Controller extends HttpServlet {
 	}
 
 	private String getOpleidingenOverzicht(HttpServletRequest request, HttpServletResponse response) {
-<<<<<<< HEAD
-//		ArrayList<Afdeling> afdelingen = new ArrayList<>();
-//		
-//		Afdeling a1 = new Afdeling("Lerarenopleiding");
-//		a1.addOpleiding(new Opleiding("Kleuteronderwijs", 1));
-//		a1.addOpleiding(new Opleiding("Lager onderwijs", 2));
-//		
-//		Afdeling a2 = new Afdeling("Gezondheid");
-//		a2.addOpleiding(new Opleiding("Mondzorg", 3));
-//		a2.addOpleiding(new Opleiding("Vroedkunde", 4));
-//		
-//		Afdeling a3 = new Afdeling("Welzijn");
-//		a3.addOpleiding(new Opleiding("Sociaal werk", 5));
-//		
-//		afdelingen.add(a1);
-//		afdelingen.add(a2);
-//		afdelingen.add(a3);
-		
 		request.setAttribute("afdelingen", afdelingDb.getAfdelingen());
 		
-=======
-
-		Afdeling a1 = new Afdeling("Lerarenopleiding");
-		a1.addOpleiding(new Opleiding("Kleuteronderwijs", 1));
-		a1.addOpleiding(new Opleiding("Lager onderwijs", 2));
-
-		Afdeling a2 = new Afdeling("Gezondheid");
-		a2.addOpleiding(new Opleiding("Mondzorg", 3));
-		a2.addOpleiding(new Opleiding("Vroedkunde", 4));
-
-		Afdeling a3 = new Afdeling("Welzijn");
-		a3.addOpleiding(new Opleiding("Sociaal werk", 5));
-
-		afdelingen.add(a1);
-		afdelingen.add(a2);
-		afdelingen.add(a3);
-
-		request.setAttribute("afdelingen", afdelingen);
-
->>>>>>> bb890df73486a715a19438057639c1cf37b311c9
 		return "opleidingOverzicht.jsp";
 	}
 }
--- src/main/java/domain/DomainException.java
+++ src/main/java/domain/DomainException.java
@@ -1,24 +1,15 @@
 package domain;
 
 public class DomainException extends RuntimeException {
-<<<<<<< HEAD
-	public DomainException() {
-		super();
-	}
-	public DomainException(String message) {
-		super(message);
-=======
+
 	private static final long serialVersionUID = 1L;
 
 	public DomainException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public DomainException(String string) {
 		super(string);
-		// TODO Auto-generated constructor stub
->>>>>>> bb890df73486a715a19438057639c1cf37b311c9
 	}
 
 }
